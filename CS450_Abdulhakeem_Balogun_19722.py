30d67ed4917d698570dc1612f88fa7f84ea0ecea
Kimburg1 <114509601+Kimburg1@users.noreply.github.com>
2022-10-06 18:18:07 -0700

    Update CS450_Abdulhakeem_Balogun_19722.py

--- CS450_Abdulhakeem_Balogun_19722.py
+++ CS450_Abdulhakeem_Balogun_19722.py
@@ -104,3 +104,111 @@ return n*cal(n-2) # calling with 2 less n
 print(cal(5)) # 15
 
 print(cal(8)) # 0
+
+
+QUESTION 6def checking(n):
+    if n<=0:
+        print("Number should be positive integer ")
+        return 
+    else: 
+      while (n>=1):
+          a = n%10
+          b = (n//10)%10
+          if a >= b:
+              output = True 
+              n//=10
+          else:
+             output = False
+             break 
+    return output 
+print(checking(5))
+print(checking(11))
+print(checking(127))
+print(checking(1357))
+print(checking(21))
+result = checking(1372)
+print(result)
+
+
+QUESTION 7
+
+def   cal(n):
+    if n%2 == 0 and n <= 1:
+      return 0
+    elif n%2 == 1 and n <= 1:
+      return 1
+
+    elif n%2 == 0:
+      return n*cal(n-2)
+    return n*cal(n-2)
+
+print(cal(5))
+
+QUESTION 8
+
+def incr(n):
+    return n+1
+def triple(n):
+    return 3*n
+def square(n):
+  return n*n
+
+def intscts(f,n):
+    if n == 1:
+       if f(n) == g(n):
+         return True
+       else:
+         return False 
+    g = lambda x: f(intscts(f, n-1)(x))
+
+at3 = intscts(square,3)
+at3(triple)
+
+
+QUESTION 9
+
+def  A(n): 
+    if n <= 3:
+      return n
+    else:
+      return A(n - 1) + 2 * A(n - 2) + 3 * A(n - 3)
+
+print(A(1))
+print(A(2))
+print(A(3))
+print(A(4))
+print(A(5))
+
+QUESTION 10
+
+def bnc_bck_frth(n):
+    k = 1
+    assending = True 
+    num_found = False 
+    for i in range (1, n):
+        #print(k)
+        j = i
+        while(j!=0):
+          if i%10 ==7:
+            num_found = True 
+            break
+          j //=10
+        #print(num_found)
+        if i%7 == 0 or num_found==True:
+            num_found = False
+            if assending == True: 
+                assending = False 
+            else:
+                assending = True 
+        if assending == True:
+            k +=1
+        else:
+            k -=1
+    return k
+print(bnc_bck_frth (7))
+print(bnc_bck_frth (8))
+print(bnc_bck_frth (15))
+print(bnc_bck_frth (21))
+print(bnc_bck_frth (22))
+print(bnc_bck_frth (30))
+print(cal(8))
